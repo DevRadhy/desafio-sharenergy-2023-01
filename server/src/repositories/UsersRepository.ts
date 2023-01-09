dbb43fd6bf7a9b370222a02749a855b3a98f4043
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-01-09 14:20:19 -0300

    Change method name

--- server/src/repositories/UsersRepository.ts
+++ server/src/repositories/UsersRepository.ts
@@ -1,7 +1,7 @@
 import User from "../entities/User";
 
 abstract class UserRepository {
-  abstract findMany(): Promise<User[]>;
+  abstract getUsers(): Promise<User[]>;
   abstract findById(id: string): Promise<User>;
   abstract create(user: User): Promise<User>;
   abstract save(user: User): Promise<User>;
