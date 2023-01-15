cdefd78640c6fa7ffdb0efdfe839efb0d06889f5
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-01-14 21:12:02 -0300

    Adding Prisma ORM

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,25 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import routes from "./routes";
+import { AppError } from "./errors/AppError";
+
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      message: error.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "Error",
+    message: `Internal server error ${error.message}`
+  });
+});
 
 export default app;
