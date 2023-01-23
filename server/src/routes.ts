e1d51361c2fc456fb7230276ef7546392bd5215d
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-01-23 17:58:51 -0300

    Create Users Routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,33 @@
 import { Router } from "express";
+import {
+  createUserController,
+  deleteUserController,
+  updateUserController,
+  getUsersController,
+} from "./controllers/Users";
 
 const routes = Router();
+
+routes.get("/client/:id", (request, response) => {
+  return updateUserController.handle(request, response);
+});
+
+routes.get("/clients/:id", (request, response) => {
+  return getUsersController.handle(request, response);
+});
+
+routes.post("/client", (request, response) => {
+  return createUserController.handle(request, response);
+});
+
+routes.patch("/client/:id", (request, response) => {
+  return updateUserController.handle(request, response);
+});
+
+routes.delete("/client/:id", (request, response) => {
+  return deleteUserController.handle(request, response);
+});
+
+
 
 export default routes;
